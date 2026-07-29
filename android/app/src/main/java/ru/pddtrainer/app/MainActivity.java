2e839982d441e329aaf000824f2af674ada4f607
Sergey <pprikol742@gmail.com>
2026-07-29 04:40:47 +0300

    fix: remove YandexAdsPlugin from MainActivity, use JS ads instead

--- android/app/src/main/java/ru/pddtrainer/app/MainActivity.java
+++ android/app/src/main/java/ru/pddtrainer/app/MainActivity.java
@@ -1,13 +1,7 @@
 package ru.pddtrainer.app;
 
 import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(YandexAdsPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
 }
